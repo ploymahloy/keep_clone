564454ba40399a69ede8052732d9167e0c16ce53
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-10-19 21:16:33 -0400

    basic UI complete (minus mobile layout)

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,51 @@ import react from './assets/react.png';
 import sass from './assets/sass.png';
 import typescript from './assets/typescript.png';
 
+const DUMMY_NOTES = [
+	{
+		title: 'Note Title 1',
+		body: '1 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+	},
+	{
+		title: 'Note Title 2',
+		body: '2 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+	},
+	{
+		title: 'Note Title 3',
+		body: '3 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+	}
+];
+
 function App() {
-  return (
+	return (
 		<div className={styles.App}>
 			<div className={styles.stack}>
-				<img className={styles.stack_icon} src={react} />
+				<img className={styles.stack_icon} src={react} alt="" />
 				{' + '}
-				<img className={styles.stack_icon} src={typescript} />
+				<img className={styles.stack_icon} src={typescript} alt="" />
 				{' + '}
-				<img className={styles.stack_icon} src={sass} />
+				<img className={styles.stack_icon} src={sass} alt="" />
 				{' + '}
-				<img className={styles.stack_icon} src={mongodb} />
+				<img className={styles.stack_icon} src={mongodb} alt="" />
 				{' = '}
-				<img className={styles.stack_icon} src={keep} />
-      </div>
-      <form className={styles.form}>
-        <input className={styles.form_title} placeholder="Title" />
-        <input className={styles.form_body} placeholder="Take a note..." />
-      </form>
+				<img className={styles.stack_icon} src={keep} alt="" />
+			</div>
+			<main>
+				<form className={styles.form}>
+					<input className={styles.form_title} placeholder="Title" />
+					<input className={styles.form_body} placeholder="Take a note..." />
+				</form>
+				<section className={styles.notes}>
+					{DUMMY_NOTES.map((note) => {
+						return (
+							<div className={styles.notes_card}>
+								<p className={styles.notes_card_title}>{note.title}</p>
+								<p className={styles.notes_card_body}>{note.body}</p>
+							</div>
+						);
+					})}
+				</section>
+			</main>
 		</div>
 	);
 }
