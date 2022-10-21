16e79f897a6b687f699af26bffa659b4b6571076
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-10-20 21:21:53 -0400

    form works, cleaned up UI to better match original Keep

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,91 @@
 import { useState } from 'react';
-import styles from './styles/App.module.scss';
 
+import styles from './styles/App.module.scss';
 import keep from './assets/keep.png';
-import mongodb from './assets/mongodb.png';
-import react from './assets/react.png';
-import sass from './assets/sass.png';
-import typescript from './assets/typescript.png';
 
 const DUMMY_NOTES = [
 	{
-		title: 'Note Title 1',
-		body: '1 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+		title: 'Note Title',
+		body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum inventore, recusandae perferendis ab tenetur ducimus ea maiores! Consectetur ipsam harum, officiis eius quisquam mollitia reiciendis, asperiores, similique dolorem placeat ut.'
 	},
 	{
-		title: 'Note Title 2',
-		body: '2 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+		title: 'Note Title',
+		body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, odio harum praesentium mollitia animi veritatis aut perspiciatis voluptate quod nostrum minima distinctio repellat asperiores impedit saepe deserunt quia doloribus sed.'
 	},
 	{
-		title: 'Note Title 3',
-		body: '3 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+		title: 'Note Title',
+		body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate harum et exercitationem rerum eligendi reprehenderit adipisci corporis. Ea fugiat voluptatibus ad suscipit eius. Ut expedita sint amet quo ad necessitatibus.'
 	},
 	{
-		title: 'Note Title 4',
-		body: '4 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+		title: 'Note Title',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur distinctio odit facere molestiae neque vero harum minus qui voluptatem. Enim, modi excepturi? Adipisci assumenda nisi provident consequuntur officia praesentium magnam!'
+    
 	},
 	{
-		title: 'Note Title 5',
-		body: '5 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+		title: 'Note Title',
+		body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit labore commodi facere magni nobis harum, totam incidunt et quibusdam magnam sed obcaecati dolorum consequatur, corporis nisi aperiam id corrupti! Numquam.'
 	},
 	{
-		title: 'Note Title 6',
-		body: '6 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nam qui odit atque dolorum nemo adipisci, modi a ducimus dolor!'
+		title: 'Note Title',
+		body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit non numquam distinctio id excepturi perferendis soluta illo a architecto doloribus quos ipsam enim facilis eligendi, magni magnam in exercitationem laudantium?'
 	}
 ];
 
 function App() {
+	const [notes, setNotes] = useState(DUMMY_NOTES);
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+
+	const handleNewTitle = (e: any) => {
+		setTitle(e.target.value);
+	};
+
+	const handleNewBody = (e: any) => {
+		setBody(e.target.value);
+	};
+
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault();
+    // setNotes([...DUMMY_NOTES, { title, body }]);
+    setNotes((DUMMY_NOTES) => {
+			return [{title, body}, ...DUMMY_NOTES];
+    });
+    
+		setTitle('');
+		setBody('');
+	};
+
 	const formKeyPressHandler = (e: any) => {
 		if (e.key === 'Enter') {
-			// Push values to array
-			alert('Enter');
+			handleFormSubmit(e);
 		} else return;
 	};
 
 	return (
 		<div className={styles.App}>
-			<div className={styles.stack}>
-				<img className={styles.stack_icon} src={react} alt="" />
-				{' + '}
-				<img className={styles.stack_icon} src={typescript} alt="" />
-				{' + '}
-				<img className={styles.stack_icon} src={sass} alt="" />
-				{' + '}
-				<img className={styles.stack_icon} src={mongodb} alt="" />
-				{' = '}
-				<img className={styles.stack_icon} src={keep} alt="" />
+			<div className={styles.brand}>
+				<img className={styles.brand_icon} src={keep} alt="" />
+				Persist
 			</div>
-			<main>
+			<main className={styles.main}>
 				<form className={styles.form} onKeyPress={formKeyPressHandler}>
-					<input className={styles.form_title} placeholder="Title" />
-					<input className={styles.form_body} placeholder="Take a note..." />
+					<input
+						className={styles.form_title}
+						placeholder="Title"
+						onChange={handleNewTitle}
+						value={title}
+					/>
+					<input
+						className={styles.form_body}
+						placeholder="Take a note..."
+						onChange={handleNewBody}
+						value={body}
+					/>
 				</form>
 				<section className={styles.notes}>
-					{DUMMY_NOTES.map((note) => {
+					{notes.map((note, id) => {
 						return (
-							<div className={styles.notes_card}>
+							<div className={styles.notes_card} key={id}>
 								<p className={styles.notes_card_title}>{note.title}</p>
 								<p className={styles.notes_card_body}>{note.body}</p>
 							</div>
