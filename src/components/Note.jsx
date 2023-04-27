48129c8513588fdb897427bdaa6d88ac797ac205
Patrick Mahloy <patrick.mahloy@gmail.com>
2023-04-27 17:12:48 -0400

    removed faulty modal edit logic to fix modal bug

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -8,41 +8,29 @@ const Note = (props) => {
 	const { id, title, body, deleteNote } = props;
 
   const [isOpen, setIsOpen] = useState(false);  
-  const [noteTitle, setNoteTitle] = useState(title);
-  const [noteBody, setNoteBody] = useState(body);
-
-  const handleEditTitle = (val) => {
-    setNoteTitle(val);
-  }
-
-  const handleEditBody = (val) => {
-    setNoteBody(val);
-  }
 
 	return (
-		<div className={styles.notes_card} key={id}>
+		<div className={styles.card} key={id}>
 			<div onClick={() => setIsOpen(true)}>
-				<p className={styles.notes_card_title}>{title}</p>
-				<p className={styles.notes_card_body}>{body}</p>
+				<p className={styles.card_title}>{title}</p>
+				<p className={styles.card_body}>{body}</p>
 			</div>
-			<div className={styles.notes_card_footer}>
+			<div className={styles.card_footer}>
 				<div
-					className={styles.notes_card_footer_gap}
+					className={styles.card_footer_gap}
 					onClick={() => setIsOpen(true)}
 				>
 				</div>
 				<button
-					className={styles.notes_card_footer_btn}
+					className={styles.card_footer_btn}
 					onClick={() => deleteNote(id)}
 				>
 					<FaTrash />
 				</button>
 			</div>
 			<Modal
-				title={noteTitle}
-        body={noteBody}
-        editTitle={handleEditTitle}
-        editBody={handleEditBody}
+				title={title}
+        body={body}
 				open={isOpen}
 				onClose={() => setIsOpen(false)}
 			/>
